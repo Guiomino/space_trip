59235d325980d54b3c9926d51e4639177f2bbf9c
Guillaume <estrade.guillaume.simplon@gmail.com>
2023-11-13 09:08:22 +0100

    feat(navbar): add navbar whitout url + home hero

--- migrations/Version20231111164621.php
+++ migrations/Version20231111164621.php
@@ -10,7 +10,7 @@ use Doctrine\Migrations\AbstractMigration;
 /**
  * Auto-generated Migration: Please modify to your needs!
  */
-final class Version20231111131909 extends AbstractMigration
+final class Version20231111164621 extends AbstractMigration
 {
     public function getDescription(): string
     {
