e2c67bf6a5116cd80a71c7ac0629b836b40e3a3f
Guillaume <estrade.guillaume.simplon@gmail.com>
2023-11-28 12:04:03 +0100

    refactor(style): composer app.js

---
+++ assets/app.js
@@ -0,0 +1,16 @@
+/*
+ * Welcome to your app's main JavaScript file!
+ *
+ * We recommend including the built version of this JavaScript file
+ * (and its CSS file) in your base layout (base.html.twig).
+ */
+
+// any CSS you import will output into a single css file (app.css in this case)
+import './styles/app.css';
+import './styles/register.css';
+import './styles/login.css';
+import './styles/stay.css';
+import './styles/booking.css';
+import './styles/resort.css';
+import './styles/dashboard.css';
+import './styles/user_profile.css';
